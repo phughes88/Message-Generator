93c658c8683c167fcd72ac7401776b3723316bbd
Paul <phughes47@gmail.com>
2021-10-12 19:24:09 +0100

    First commit, message arrays added

---
+++ D&D message.js
@@ -0,0 +1,3 @@
+let firstMessage = ['strength', 'intelligence', 'wisdom', 'charisma', 'dexterity', 'constitution']
+let secondMessage = ['saving throw', 'check', 'stright roll']
+let thirdMessage = [1, 10, 15, 20]
