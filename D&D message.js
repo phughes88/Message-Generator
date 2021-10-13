214792bf16d1a91c5e5284d3ba22248a79b9a9bc
Paul <phughes47@gmail.com>
2021-10-13 19:13:10 +0100

    Generator completed

--- D&D message.js
+++ D&D message.js
@@ -1,11 +1,12 @@
-let firstMessage = ['strength', 'intelligence', 'wisdom', 'charisma', 'dexterity', 'constitution']
-let secondMessage = ['saving throw', 'check', 'stright roll']
-let thirdMessage = [1, 10, 15, 20]
+let firstMessage = ['strength', 'intelligence', 'wisdom', 'charisma', 'dexterity', 'constitution'];
+let secondMessage = ['saving throw', 'check', 'stright roll'];
+let thirdMessage = [1, 10, 15, 20];
 
-let firstSelection = firstMessage[Math.floor(Math.random()*firstMessage.length)]
-let secondSelection = secondMessage[Math.floor(Math.random()*secondMessage.length)]
-let thirdSelection = thirdMessage[Math.floor(Math.random()*thirdMessage.length)]
+let firstSelection = firstMessage[Math.floor(Math.random()*firstMessage.length)];
+let secondSelection = secondMessage[Math.floor(Math.random()*secondMessage.length)];
+let thirdSelection = thirdMessage[Math.floor(Math.random()*thirdMessage.length)];
+
+const randomMessage = (`Roll a ${firstSelection} ${secondSelection}, you rolled a ${thirdSelection}`);
 
 
-
-
+console.log (randomMessage);
