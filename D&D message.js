e4b0fc635dbb1573c413b522e71f673e2b91d01f
Paul <phughes47@gmail.com>
2021-10-13 19:06:32 +0100

    Random generator added

--- D&D message.js
+++ D&D message.js
@@ -1,3 +1,11 @@
 let firstMessage = ['strength', 'intelligence', 'wisdom', 'charisma', 'dexterity', 'constitution']
 let secondMessage = ['saving throw', 'check', 'stright roll']
 let thirdMessage = [1, 10, 15, 20]
+
+let firstSelection = firstMessage[Math.floor(Math.random()*firstMessage.length)]
+let secondSelection = secondMessage[Math.floor(Math.random()*secondMessage.length)]
+let thirdSelection = thirdMessage[Math.floor(Math.random()*thirdMessage.length)]
+
+
+
+
